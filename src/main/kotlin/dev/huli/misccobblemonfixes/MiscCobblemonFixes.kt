695c08106dfe28cf6615f5d105e8a1ef856acd71
D. Dujmovic <deniel@dujmovic.at>
2023-11-17 21:58:23 +0100

    Added a check for multiple Mega Pokemon

--- src/main/kotlin/dev/huli/misccobblemonfixes/MiscCobblemonFixes.kt
+++ src/main/kotlin/dev/huli/misccobblemonfixes/MiscCobblemonFixes.kt
@@ -3,10 +3,14 @@ package dev.huli.misccobblemonfixes
 
 import com.cobblemon.mod.common.api.events.CobblemonEvents
 import com.cobblemon.mod.common.api.events.battles.BattleStartedPostEvent
+import com.cobblemon.mod.common.api.events.battles.BattleStartedPreEvent
 import com.cobblemon.mod.common.api.events.battles.BattleVictoryEvent
 import com.cobblemon.mod.common.api.events.pokemon.evolution.EvolutionAcceptedEvent
 import com.cobblemon.mod.common.api.events.pokemon.evolution.EvolutionCompleteEvent
+import com.cobblemon.mod.common.api.pokemon.PokemonProperties.Companion.parse
 import com.cobblemon.mod.common.config.CobblemonConfig
+import com.cobblemon.mod.common.platform.events.PlatformEvents
+import com.cobblemon.mod.common.platform.events.ServerEvent
 import com.cobblemon.mod.common.pokemon.Pokemon
 import com.cobblemon.mod.common.pokemon.Species
 import com.cobblemon.mod.common.pokemon.abilities.HiddenAbilityType
@@ -23,13 +27,19 @@ import dev.huli.misccobblemonfixes.util.PokemonPolymerItem
 import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
 import net.minecraft.command.CommandRegistryAccess
+import net.minecraft.server.MinecraftServer
 import net.minecraft.server.command.CommandManager
 import net.minecraft.server.command.ServerCommandSource
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.text.MutableText
+import net.minecraft.text.Text
+import java.util.UUID
 
 object MiscCobblemonFixes {
     lateinit var battleHandler : BattleHandler
     lateinit var permissions: MiscFixesPermissions
     final var MOD_ID = "misccobblemonfixes"
+    lateinit var SERVER : MinecraftServer
     //lateinit var megaStoneModelData: PolymerModelData
     //lateinit var candyModelData: PolymerModelData
 
@@ -58,8 +68,9 @@ object MiscCobblemonFixes {
         CommandRegistrationCallback.EVENT.register(
             CommandRegistrationCallback { dispatcher, registryAccess, environment -> registerCommands(dispatcher,registryAccess,environment)  }
         )
-
+        PlatformEvents.SERVER_STARTED.subscribe { started: ServerEvent.Started -> SERVER = started.server  }
         //ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ItemGroupEvents.ModifyEntries { content -> addItemsToItemGroup(content) });
+        CobblemonEvents.BATTLE_STARTED_PRE.subscribe { battleStartedPreEvent -> checkTeam(battleStartedPreEvent)  }
         CobblemonEvents.BATTLE_STARTED_POST.subscribe { battleStartedPostEvent -> savePlayerTeam(battleStartedPostEvent)  }
         CobblemonEvents.BATTLE_VICTORY.subscribe { battleVictoryEvent -> postBattleItems(battleVictoryEvent)}
         CobblemonEvents.EVOLUTION_ACCEPTED.subscribe { evolutionAcceptedEvent -> preEvolution(evolutionAcceptedEvent)  }
@@ -70,6 +81,8 @@ object MiscCobblemonFixes {
 //        content.add(DYNAMAX_CANDY)
 //    }
 
+
+
     private fun registerCommands(
         dispatcher: CommandDispatcher<ServerCommandSource>,
         registry: CommandRegistryAccess,
@@ -78,6 +91,39 @@ object MiscCobblemonFixes {
         MegaEvolve().register(dispatcher)
         Dynamax().register(dispatcher)
     }
+
+
+    private fun checkTeam(event: BattleStartedPreEvent){
+        if(event.isPvP && event.participants.size == 2){
+            val megas : HashMap<UUID,Int> = HashMap()
+            event.participants.forEach { battleActor ->
+                val uuid: UUID = battleActor.uuid
+                var megaCount = 0
+                    battleActor.pokemonList.forEach { battlePokemon ->
+                    if(parse("mega=true", " ", "=").matches(battlePokemon.originalPokemon)){
+                      megaCount++
+                    }
+                }
+                megas[uuid] = megaCount
+            }
+            val firstUserWithMoreMegas: ServerPlayerEntity? = SERVER.playerManager.getPlayer(megas.firstNotNullOfOrNull { it.key.takeIf { key-> megas[key]!! > 1 } })
+            megas.forEach { (uuid, _) ->
+                if(firstUserWithMoreMegas != null){
+                    val playerEntity = SERVER.playerManager.getPlayer(uuid)
+                    if(playerEntity != firstUserWithMoreMegas){
+                            val playerName = firstUserWithMoreMegas.name
+                        playerEntity?.sendMessage(Text.literal("Multiple Megas found on ").append(playerName).append(", cancelling the battle"))
+                    }
+                    else{
+                        playerEntity.sendMessage(Text.literal("You have multiple Megas, cancelling the battle"))
+                    }
+                    if(!event.isCanceled) event.cancel()
+                }
+            }
+        }
+    }
+
+
     private fun savePlayerTeam(event: BattleStartedPostEvent){
         if(event.battle.players.isNotEmpty() && event.battle.players.size == 2){
             this.battleHandler.addBattle(event.battle.battleId,event.battle.players)
@@ -101,6 +147,7 @@ object MiscCobblemonFixes {
         val species: Species = event.pokemon.species
         val mon: Pokemon = event.pokemon
         var ha = false
+
         species.abilities.forEach { potentialAbility ->
             if(potentialAbility.template == mon.ability.template){
                 ha = potentialAbility.type == HiddenAbilityType
